69100890366b64384f092fb0a23333f451750d38
chery li <cheryli2017@outlook.com>
2021-06-11 10:37:07 +0800

    first commit

---
+++ lid_driven_cavity/lid_driven_cavity.c
@@ -0,0 +1,12 @@
+/* this is a lattice boltzmann method code to solve a 2D lid driven cavity problem
+   D2Q9 with single relaxation time
+   author: Btli
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define NX 128
+
+const pi = 3.1415926;
